1067a6e650e9980d6332c7c2a419df299b418985
Samuel <Samuel.joly@laplateforme.io>
2019-10-28 11:45:15 +0100

    end of php for job1 and job0

---
+++ Jour1/Job1/index.php
@@ -0,0 +1,19 @@
+<?php
+
+$str='Hello LaPlateforme';
+echo $str;
+$str2 = 'Vive ';
+$str3 = ' !';
+echo $str2.substr($str,5).$str3;
+
+$val = 6;
+echo $val;
+$val = $val + 4;
+echo $val;
+
+$myBool = true;
+echo $myBool;
+$myBool = false;
+echo $myBool;
+
+?>
