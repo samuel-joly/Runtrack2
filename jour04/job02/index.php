aa575e175d5a0dadfc0f378f74b0305e9955724b
Samuel <Samuel.joly@laplateforme.io>
2019-10-31 11:25:51 +0100

    End of job02

---
+++ jour04/job02/index.php
@@ -0,0 +1,36 @@
+<form action="index.php" method="get" style="display:flex;
+											flex-direction:column;
+											width:200px;">
+	<label for="Nom">Nom</label>
+	<input type="text" name="Nom"/>
+	
+	<label for="Prenom">Prenom</label>
+	<input type="text" name="Prenom"/>
+	
+	<label for="eMail">Mail</label>
+	<input type="mail" name="eMail"/>
+	
+	<label for="Tel">Téléphone</label>
+	<input type="tel" name="Tel"/>
+	
+	<input type="submit"/>
+</form>
+
+
+<?php
+
+	echo "<table>
+				<thead>
+					<th>Argument</th>
+					<th>Valeur</th>				
+				</thead>
+				
+				<tbody>";
+				
+	foreach($_GET as $args => $value)
+	{
+		echo "<tr><td>".$args."</td><td>".$value."</td></tr>";
+	}
+	
+	echo "</tbody>";
+?>
