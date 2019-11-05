c0e74caa1ab394c5331d6d178696b3993ecf75e6
Samuel <Samuel.joly@laplateforme.io>
2019-11-05 11:11:34 +0100

    End of job06

---
+++ jour05/job06/index.php
@@ -0,0 +1,55 @@
+<?php
+
+	/*
+	
+		les “A” deviennent des “4”, les “B” des “8”,
+		les “E” des “3”, les “G” des “6”, les “L” des “1”, les “S” des “5” et les “T” des
+		“7”.
+	
+	*/
+	
+	function leetspeak($str)
+	{
+		$count = 0;
+		while($count < strlen($str))
+		{
+			
+			if ($str[$count] == "a" || $str[$count] == "A")
+			{
+				$str[$count] = "4";
+			}
+			else if ($str[$count] == "b" || $str[$count] == "B")
+			{
+				$str[$count] = "8";
+			}			
+			else if ($str[$count] == "e" || $str[$count] == "E")
+			{
+				$str[$count] = "3";
+			}
+			else if ($str[$count] == "g" || $str[$count] == "G")
+			{
+				$str[$count] = "6";
+			}
+			else if ($str[$count] == "l" || $str[$count] == "L")
+			{
+				$str[$count] = "1";
+			}
+			else if ($str[$count] == "s" || $str[$count] == "S")
+			{
+				$str[$count] = "5";
+			}
+			else if ($str[$count] == "t" || $str[$count] == "T")
+			{
+				$str[$count] = "7";
+			}
+			
+			$count ++;
+		}
+		
+		return $str;
+	}
+	
+	
+	echo leetspeak("Je parle en leet tous les jours Gare a vous !");
+
+?>
